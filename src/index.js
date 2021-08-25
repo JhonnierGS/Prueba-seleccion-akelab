fd8c2f3ad683399654df50a3a14646a75cd51c53
jhonnieGS <gs.170590@gmail.com>
2021-08-25 17:09:15 -0500

    agregando react redux al proyecto

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
 import App from './components/App'
+import reducers from './reducers';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 
-
+const store = createStore (
+    reducers,//todos los reducer
+    {},// estado inicial
+    applyMiddleware(reduxThunk)
+)
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+    <Provider store={ store }>
+        <App /> 
+    </Provider>,
+    container
+);
